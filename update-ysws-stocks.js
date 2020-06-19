a8245aaf54e27c3720c4911ae3b6d2320eaf29e3
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-06-19 18:04:18 +0200

    Hotfix: reverted changes (won't work with API)

--- update-ysws-stocks.js
+++ update-ysws-stocks.js
@@ -1,10 +1,8 @@
 module.exports.handler = async (event) => {
   const getLastUpdatedData = require('./helpers/get-last-updated-data.js')
   const getStockData = require('./helpers/get-stock-data.js')
-  // const setStockData = require('./helpers/set-stock-data.js')
-  const getApiResponse = require('./helpers/get-api-response.js')
+  const setStockData = require('./helpers/set-stock-data.js')
   return await getLastUpdatedData()
   .then(getStockData)
-  .then(getApiResponse)
-  // .then(stocks => setStockData({ idType: 'EAN', warehouse: 'ysws', stockData: stocks }))
+  .then(stocks => setStockData({ idType: 'EAN', warehouse: 'ysws', stockData: stocks }))
 }
